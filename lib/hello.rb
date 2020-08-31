8f1455eecde03099539f1ccf1602977fde8c26c0
April Adams <illusionlace@gmail.com>
2020-08-31 15:43:53 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,12 +5,12 @@ def hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
   def hello_t(array)
   i = 0
- 
+
   while i < array.length
     yield(array[i])
     i = i + 1
   end
- 
+
   array
 end
 # call your method here!
