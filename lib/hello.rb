c999d17c5c12cd3eb09f3c18d791bb107056b527
April Adams <illusionlace@gmail.com>
2020-08-31 15:40:37 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,12 @@
-def hello_t
+def hello_t(array)
+  array.each do |name| 
+    if name.starts_with?("T")
+      puts "Hi, #{name}"
+    end 
+  end 
 
 end
 
 # call your method here!
-def hello_t(array)
-  i = 0
 
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
 
-  array
-end
